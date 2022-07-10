a4507a30d4c8485345ce5d078cda8f4b693a6867
fffonceca <fffonseca@uc.cl>
2022-07-09 22:15:55 -0400

    pid calc done

--- foca/prueba.py
+++ foca/prueba.py
@@ -3,39 +3,48 @@ import gym  # OpenAI gym
 import glfw
 
 
-def pid_calculation(pid_wights: np.array,
-                    u_k_1: float, e_k_v: np.array, Ts=0.01) -> float:
+def pid_calc(pid_wights1: np.array, e_k_v_1: np.array,
+             pid_wights2: np.array, e_k_v_2: np.array,
+             torques: np.array, Ts=0.01) -> tuple:
     """
-    Calculate new actuator
+    Calculate new actuators value
     args:
         pid_wights = [Kp, Ki, Kd]
         u_k_1: actuador anterior
         e_k = [e_k, e_k_1, e_k_2]
 
     return new actuator value
-        u_new -> float
+        (u_new1, u_new2) -> (float, float)
     """
-    # despackaging e_k
-    e_k, e_k_1, e_k_2 = e_k_v
-
+    u_prev1, u_prev2 = torques
+    # Torque 2
+    # depackaging e_k
+    e_k, e_k_1, e_k_2 = e_k_v_1
     # despackaging PID
-    Kp, Ki, Kd = pid_wights
+    Kp, Ki, Kd = pid_wights1
+    u_new1 = u_prev1 + Kp*(e_k-e_k_1) + Ki*e_k*Ts + Kd*(e_k-2*e_k_1+e_k_2)/Ts
 
-    u_k = u_k_1 + Kp*(e_k-e_k_1) + Ki*e_k*Ts + Kd*(e_k-2*e_k_1+e_k_2)/Ts
+    # Torque 2
+    # depackaging e_k
+    e_k, e_k_1, e_k_2 = e_k_v_2
+    # despackaging PID
+    Kp, Ki, Kd = pid_wights2
+    u_new2 = u_prev2 + Kp*(e_k-e_k_1) + Ki*e_k*Ts + Kd*(e_k-2*e_k_1+e_k_2)/Ts
 
-    return u_k
+    return np.array([u_new1, u_new2])
 
 
 def main():
-    # Torque 1 params
-    u_prev1 = 0
-    error_vector1 = np.array([0.0, 0.0, 0.0])
-    pid_constants1 = np.array([1.0, 0.2, 0.3])
-    # Torque 2 params
-    u_prev2 = 0
-    error_vector2 = np.array([0.0, 0.0, 0.0])
-    pid_constants2 = np.array([1.0, 0.2, 0.3])
-
+    # PARAMS
+    Ts = 0.01
+    # PID
+    pid_k1 = np.array([1.0, 0.2, 0.3])
+    pid_k2 = np.array([1.0, 0.2, 0.3])
+    # prev
+    torques = np.array([0.0, 0.0])
+    # Errors
+    err_v1 = np.array([0.0, 0.0, 0.0])
+    err_v2 = np.array([0.0, 0.0, 0.0])
 
     # Para visualizar que es lo que hace el agente
     glfw.init()
@@ -50,14 +59,12 @@ def main():
         env.render()
         accion = env.action_space.sample()
 
-        observation, reward, done, aux_dict = env.step([u_prev1, u_prev2])
+        observation, reward, done, aux_dict = env.step(torques)
 
-        # # PID calc of torque 1
-        # u_prev1 = pid_calculation(pid_constants1, u_prev1, error_vector1)
-        # # PID calc of torque 2
-        # u_prev2 = pid_calculation(pid_constants2, u_prev2, error_vector2)
+        # PID calc
+        torques = pid_calc(pid_k1, err_v1, pid_k2, err_v2, torques, Ts)
 
-        print(len(observation))
+        print(observation[0], observation[1])
 
     glfw.terminate()
     env.close()
