5d2bd03cbc3613548cd8695ce07b77bf9ebd5ed7
fffonceca <fffonseca@uc.cl>
2022-07-10 22:02:13 -0400

    main_lqi, matrices completas

--- lib/modelo.py
+++ lib/modelo.py
@@ -1,11 +1,34 @@
+from scipy.linalg import expm
 import numpy as np
 
 
-def jacobiano(observation: np.array, u: np.array,
-              m=0.1*0.01**2*np.pi*1000, L1=0.1, L2=0.1):
+def matrices_completas(obs: np.array, torques: np.array):
+    """
+    Recibe el vector de observaciones, los torques y se obtienen
+    las matrices completras para el LQI
+    """
+    # Matrices continuas
+    A, B, C, D = jacobianos(obs, torques)
+    # Matrices discretas
+    Ad, Bd, Cd = discretizar_jacobianos(A, B, C)
+    # Matrices aumentadas
+    Aa, Ba, Ca = ampliar_matrices_LQI(Ad, Bd, Cd, D)
+
+    return Aa, Ba, Ca
+
+
+def jacobianos(observation: np.array, u: np.array,
+               m=0.1*0.01**2*np.pi*1000, L1=0.1, L2=0.1):
+    """
+    Entrega las matrices en el espacio continuo
+    """
+    # Depackaging obs
     obs0, obs1, obs2, obs3, _, _, obs6, obs7, _, _, _,  = observation
+
+    # Matriz A
     A = np.array([])
 
+    # Matriz B
     den_b = L1**2*m*(16*L1**2+12*L1*L2*obs1+3*L2**2*(17-12*obs1**2))
     B = np.array([
         [0., 0.],
@@ -14,31 +37,42 @@ def jacobiano(observation: np.array, u: np.array,
         [-36*L2*(L2+2*L1*obs1), 36*L2**2+48*L1*(4*L1+3*L2*obs1)]
     ])/den_b
 
+    # Matriz C
     C = np.array([
         [1., 0., 0., 0.],
         [0., 1., 0., 0.]
     ])
 
+    # Matriz D
     D = np.array([
         [0., 0.],
         [0., 0.]
     ])
 
+    return A, B, C, D
 
-def matrices_aumentadas(A, B, C, D):
-    # Matriz Aa
-    aux = np.vstack((A, -C))
-    n_filas = aux.shape[0]
-    a = np.zeros((n_filas, 3))
-    Aa = np.hstack((aux, a))
 
-    # Matriz Ba
+def discretizar_jacobianos(A: np.array, B: np.array, C: np.array, Ts=0.01):
+    """
+    Discretiza las matrices
+    Continuo -> Discreto
+    """
+    Ad = expm(A*Ts)
+    Bd = ((Ad*Ts).dot(np.eye(len(Ad))-A*Ts/2.)).dot(B)
+    return Ad, Bd, C
+
+
+def ampliar_matrices_LQI(A, B, C, D):
+    """
+    Entrega a partir de las matrices A, B, C y D
+    las matrices aumentadas para el control LQI
+    """
+    A = np.vstack((A, -C))
+    n_filas = int(A.shape[0])
+    a = np.zeros((n_filas, 2))
+    Aa = np.hstack((A, a))
     Ba = np.vstack((B, -D))
+    c = np.zeros((2, 2))
+    Ca = np.hstack((C, c))
 
-    # Matriz Ca
-    Ca = 0
-    Ca = np.hstack((C, np.zeros((3, 3))))
-
-    Da = D
-
-    return Aa, Ba, Ca, Da
+    return Aa, Ba, Ca
