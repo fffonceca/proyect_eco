e93c7e076422b6266b747aa09f47e13eae4c4078
fffonceca <fffonseca@uc.cl>
2022-07-11 03:21:12 -0400

    modelo cambiado v1.1

--- lib/modelo.py
+++ lib/modelo.py
@@ -32,7 +32,17 @@ def jacobianos(observation: np.array, tau: np.array,
     # Depackaging obs
     obs0, obs1, obs2, obs3, _, _, obs6, obs7, _, _, _ = observation
 
-    COS2Q2 = obs1**2 - obs3**2
+    comun_den = (L1*(16*L1**2 + 12*L1*L2*obs1 + 3*L2**2*(17-12*obs1**2)))
+    A33_num = 12*L2*obs3*(6*L1*L2*obs1*obs6+L1**2*obs7+3*L1**2*(obs6+obs7))
+    A34_num = 12*L2*(L1**2 + 3*L2**2)*obs3*(obs6 + obs7)
+    A43_num = -(12*L2*obs3*(16*L1**2*obs6+3*L2**2*(obs6+obs7)+6*L1*L2*obs1*(
+        2*obs6+obs7)))
+    A44_num = -(36*L2**2*(L2 + 2*L1*obs1)*obs3*(obs6 + obs7))
+
+    A33 = A33_num/comun_den
+    A34 = A34_num/comun_den
+    A43 = A43_num/comun_den
+    A44 = A44_num/comun_den
 
     A32 = (6*L2*(-2*(L1-6*L2*obs1)*obs3*(
         -6*(
@@ -46,10 +56,6 @@ def jacobianos(observation: np.array, tau: np.array,
             )**2+L1**2*obs1*obs7*(2*obs6+obs7))-12*obs3*tau[1]))
     )/(L1**2*m**2*(16*L1**2+12*L1*L2*obs1+3*L2**2*(17-12*obs1**2))**2)
 
-    A33 = -1*(12*L2*obs3*(6*L1*L2*obs1*obs6+L1**2*obs7+3*L2**2*(obs6+obs7)))/(L1*(16*L1**2+33*L2**2+12*L1*L2*obs1-18*L2**2*COS2Q2))
-
-    A34 = -1*(36*L2**2*(L2*+2*L1*obs1)*obs3*(obs6+obs7)) / (L1*(16*L1**2 + 33*L2**2 + 6*L2*(2*L1*obs1 - 3*L2*COS2Q2)))
-
     A42 = (6*L2*(
         L1*m*(-256*L1**4*obs1*obs6**2+9*L2**4*obs1*(
             -17+12*obs1**2+24*obs3**2
@@ -59,8 +65,7 @@ def jacobianos(observation: np.array, tau: np.array,
             )*obs6*obs7-(2+3*obs1**2)*obs7**2
         )-96*L1**3*L2*(
             -obs3**2*obs7*(2*obs6+obs7)+obs1**2*(
-                4*obs6**2+2*obs6*obs7+obs7**2
-            )
+                4*obs6**2+2*obs6*obs7+obs7**2)
         )+18*L1*L2**3*(
             12*obs1**4*(
                 2*obs6**2+2*obs6*obs7+obs7**2
@@ -72,10 +77,6 @@ def jacobianos(observation: np.array, tau: np.array,
                 16*L1**3+36*L2**3*obs1+9*L1*L2**2*(5+4*obs1**2)
             )*obs3*tau[0]-144*L2*(L2+2*L1*obs1)*(8*L1+3*L2*obs1)*obs3*tau[1])
         )/(L1**2*m*(16*L1**2+12*L1*L2*obs1+3*L2**2*(17-12*obs1**2))**2)
-
-    A43 = -1*(12*L2*obs3*(16*L1**2*obs6+3*L2**2*(obs6+obs7) + 6*L1*L2*obs1*(2*obs6+obs7)))/(L1*(16*L1**2 + 12*L1*L2*obs1 + 3*L2**2*(17-12*obs1**2)))
-
-    A44 = -1*(36*L1**2*(L2 + 2*L1*obs1)*obs3*(obs6 + obs7)) / (L1*(16*L1**2+12*L1*L2*obs1 + 3*L2**2*(17-12*obs1**2)))
 
     # Matriz A
     A = np.array([
